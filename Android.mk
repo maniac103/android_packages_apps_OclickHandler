2bab15fbe5a91d8876a6bef781acb7c3906c8af1
Danny Baumann <dannybaumann@web.de>
2019-11-13 10:38:18 +0100

    Move app from /vendor to /product.
    Otherwise it's not available with prebuilt vendor images.
    
    Change-Id: I2061ae51ab984114b233aa1c41a443bc5d28153f

--- Android.mk
+++ Android.mk
@@ -8,6 +8,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := OclickHandler
 LOCAL_CERTIFICATE := platform
-LOCAL_VENDOR_MODULE := true
+LOCAL_PRODUCT_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
 include $(BUILD_PACKAGE)
